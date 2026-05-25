713d63703401e2685e76f87eeceaf05f98cc68cd
hunglvd <hunglvd@vingroup.net>
2026-05-25 23:00:35 +0700

    fix cuFrameDifference

--- bgslibrary/algorithms/cuFrameDifference.cu
+++ bgslibrary/algorithms/cuFrameDifference.cu
@@ -10,7 +10,7 @@ constexpr int NUM_THREADS = 256;
 __global__ void cuFrameDifference_kernel_8u_c3r(
     const uint8_t *__restrict__ img_input, const uint8_t *__restrict__ bg_input,
     uint8_t *__restrict__ img_output, int width, int height, int in_step,
-    int out_step, float threshold) {
+    int bg_step, int out_step, float threshold) {
   const auto tidx = blockIdx.x * blockDim.x + threadIdx.x;
   if (tidx >= width * height) {
     return;
@@ -19,11 +19,12 @@ __global__ void cuFrameDifference_kernel_8u_c3r(
   const int x = tidx % width;
   const int y = tidx / width;
 
-  const int ch[3] = {y * in_step + 3 * x, y * in_step + 3 * x + 1,
-                     y * in_step + 3 * x + 2};
-  float gray = 0.114f * abs(img_input[ch[0]] - bg_input[ch[0]]) +
-               0.587f * abs(img_input[ch[1]] - bg_input[ch[1]]) +
-               0.299f * abs(img_input[ch[2]] - bg_input[ch[2]]);
+  float gray = 0.299f * abs(img_input[y * in_step + 3 * x] -
+                            bg_input[y * bg_step + 3 * x]) +
+               0.587f * abs(img_input[y * in_step + 3 * x + 1] -
+                            bg_input[y * bg_step + 3 * x + 1]) +
+               0.114f * abs(img_input[y * in_step + 3 * x + 2] -
+                            bg_input[y * bg_step + 3 * x + 2]);
   img_output[y * out_step + x] = (gray > threshold) ? 255 : 0;
 }
 
@@ -38,8 +39,8 @@ void cuFrameDifference_8u_c3r(const cv::cuda::GpuMat &img_input,
 
   cuFrameDifference_kernel_8u_c3r<<<blocks, NUM_THREADS>>>(
       img_input.data, bg_input.data, img_output.data, width, height,
-      static_cast<int>(img_input.step), static_cast<int>(img_output.step),
-      threshold);
+      static_cast<int>(img_input.step), static_cast<int>(bg_input.step),
+      static_cast<int>(img_output.step), threshold);
 
   CUDA_Check(cudaGetLastError());
 }
@@ -58,13 +59,11 @@ cuFrameDifference::~cuFrameDifference() {
 
 void cuFrameDifference::process(const cv::cuda::GpuMat &img_input,
                                 cv::cuda::GpuMat &img_output) {
-  init(img_input, img_output);
-
   if (img_foreground.empty()) {
     img_foreground = cv::cuda::GpuMat(img_input.size(), CV_8UC1, cv::Scalar(0));
   }
   if (img_background.empty()) {
-    img_background = img_input;
+    img_input.copyTo(img_background);
     first_time = false;
     return;
   }
@@ -72,8 +71,8 @@ void cuFrameDifference::process(const cv::cuda::GpuMat &img_input,
   cuFrameDifference_8u_c3r(img_background, img_input, img_foreground,
                            static_cast<float>(threshold));
 
-  img_output = img_foreground;
-  img_background = img_input;
+  img_foreground.copyTo(img_output);
+  img_input.copyTo(img_background);
 }
 
 void cuFrameDifference::save_config(cv::FileStorage &fs) {
